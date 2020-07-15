2ec58fb942dba3b384ba47f4786f56b5e0421e25
Philip Helger <philip@helger.com>
2020-07-15 22:35:27 +0200

    Version bump

--- peppol-codelist-tools/src/main/java/eu/peppol/codelist/jaxb/CCodelists.java
+++ peppol-codelist-tools/src/main/java/eu/peppol/codelist/jaxb/CCodelists.java
@@ -19,7 +19,8 @@ import com.helger.commons.io.resource.ClassPathResource;
 
 final class CCodelists
 {
-  public static final ClassPathResource XSD = new ClassPathResource ("/schemas/peppol-codelists-v1.xsd", CCodelists.class.getClassLoader ());
+  public static final ClassPathResource XSD = new ClassPathResource ("/schemas/peppol-codelists-v1.xsd",
+                                                                     CCodelists.class.getClassLoader ());
 
   private CCodelists ()
   {}
