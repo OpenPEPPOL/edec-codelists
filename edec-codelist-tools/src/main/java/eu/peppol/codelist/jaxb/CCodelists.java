9ab97f4305ea6d08e9d72fa495a4f77a72fc3f45
Philip Helger <philip@helger.com>
2024-07-26 22:25:17 +0200

    Test fix

--- edec-codelist-tools/src/main/java/eu/peppol/codelist/jaxb/CCodelists.java
+++ edec-codelist-tools/src/main/java/eu/peppol/codelist/jaxb/CCodelists.java
@@ -19,7 +19,7 @@ import com.helger.commons.io.resource.ClassPathResource;
 
 final class CCodelists
 {
-  public static final ClassPathResource XSD = new ClassPathResource ("/external/schemas/peppol-codelists-v2.2.xsd",
+  public static final ClassPathResource XSD = new ClassPathResource ("/external/schemas/peppol-codelists-v2.3.xsd",
                                                                      CCodelists.class.getClassLoader ());
 
   private CCodelists ()
